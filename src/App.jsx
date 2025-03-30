997cf1b05f7a1860591011d7da09cedf1e1e8f1c
vickyy234 <vickymayilsamy@gmail.com>
2025-03-30 22:09:43 +0530

    Updated ScrollToTopComponent.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ScrollToTop from './components/ScrollToTop'
 import Login from './components/login'
 
 const App = () => {
   return (
     <>
+      <ScrollToTop/>
       <Login />
     </>
   )
