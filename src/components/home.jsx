92ee0d88cbd2fb241b42f943eb8c454588839bc9
Pradeep T M <pradeeptm5377@gmail.com>
2025-04-03 05:03:53 +0530

    update

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,8 +4,11 @@ import attendance from '../assets/calendar.png'
 import foodtracking from '../assets/nutrition-plan.png'
 import measurement from '../assets/kpi.png'
 import bill from '../assets/receipt.png'
+import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 import '../styles/home.css'
 const Home=()=>{
+    const navigate = useNavigate();
     return(
         <>
         <Header/>
@@ -14,23 +17,23 @@ const Home=()=>{
             <div className="home-main">
                 <div className="home-card">
                     <img src={beneficiaries} />
-                    <button className='home-card-btn'>Beneficiaries</button>
+                    <button className='home-card-btn' onClick={() => navigate('/benificiaries')}>Beneficiaries</button>
                 </div>
                 <div className="home-card">
                     <img src={attendance} />
-                    <button className='home-card-btn'>Attendance</button>
+                    <button className='home-card-btn'onClick={() => navigate('/attendance')}>Attendance</button>
                 </div>
                 <div className="home-card">
                     <img src={foodtracking} />
-                    <button className='home-card-btn'>Food Tracking</button>
+                    <button className='home-card-btn' onClick={() => navigate('/foodtracking')}>Food Tracking</button>
                 </div>
                 <div className="home-card">
                     <img src={measurement} />
-                    <button className='home-card-btn'>Measurement</button>
+                    <button className='home-card-btn' onClick={() => navigate('/measurement')}>Measurement</button>
                 </div>
                 <div className="home-card">
                     <img src={bill} />
-                    <button className='home-card-btn'>Recipts/Bills</button>
+                    <button className='home-card-btn' onClick={() => navigate('/bills')}>Recipts/Bills</button>
                 </div>
             </div>
 
