1d416afd37ca892c8036fed62fee3fd9bb7b957a
VISHALKANNAN070 <vishalkannan070@gmail.com>
2025-04-03 09:37:42 +0530

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,37 +5,28 @@ import foodtracking from '../assets/nutrition-plan.png'
 import measurement from '../assets/kpi.png'
 import bill from '../assets/receipt.png'
 import { Link } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom';
 import '../styles/home.css'
 const Home=()=>{
-    const navigate = useNavigate();
+    const menu=[{img:beneficiaries, name:"Beneficiaries",link:"beneficiaries"},
+        {img:attendance, name:"Attendance",link:"attendance"},
+        {img:foodtracking, name:"Food Tracking",link:"foodtracking"},
+        {img:measurement, name:"Measurement",link:"measurement"},
+        {img:bill, name:"Recipts/Bills",link:"bill"}
+    ]
     return(
         <>
         <Header/>
         <div className="home-container">
             <p>Welcome to <span>Child Nutrition Tracker</span></p>
-            <div className="home-main">
-                <div className="home-card">
-                    <img src={beneficiaries} />
-                    <button className='home-card-btn' onClick={() => navigate('/benificiaries')}>Beneficiaries</button>
-                </div>
-                <div className="home-card">
-                    <img src={attendance} />
-                    <button className='home-card-btn'onClick={() => navigate('/attendance')}>Attendance</button>
-                </div>
-                <div className="home-card">
-                    <img src={foodtracking} />
-                    <button className='home-card-btn' onClick={() => navigate('/foodtracking')}>Food Tracking</button>
-                </div>
-                <div className="home-card">
-                    <img src={measurement} />
-                    <button className='home-card-btn' onClick={() => navigate('/measurement')}>Measurement</button>
-                </div>
-                <div className="home-card">
-                    <img src={bill} />
-                    <button className='home-card-btn' onClick={() => navigate('/bills')}>Recipts/Bills</button>
-                </div>
-            </div>
+{menu.map((item,index)=>(
+    <div key={index} className="home-main">
+        <div className="home-card">
+            <img src={item.img} alt={item.name} />
+            <Link to='/{item.link}'><button className='home-card-btn'>{item.name}</button></Link>
+        </div>
+    </div>
+))}
+
 
         </div>
         </>
