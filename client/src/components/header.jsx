d78d7b7a1720d26f21b7be922ccebe712abe93b4
Vishal Kannan <vishalkannan070@gmail.com>
2025-06-25 20:51:41 +0530

    logout change

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,44 +1,5 @@
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-<<<<<<< Updated upstream
-import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
-import axios from 'axios';
-import '../styles/header.css'
-import { Link, useNavigate } from "react-router-dom";
-
-const Header = ({ userName, setUserName }) => {
-
-    const navigate = useNavigate();
-    const handleLogout = async () => {
-        try {
-            const confirmLogout = window.confirm("Are you sure you want to logout?")
-            if (!confirmLogout) return;
-
-            const res = await axios.post('/auth/logout')
-            alert(res.data.message || "Logout successful")
-            setUserName(null)
-            sessionStorage.removeItem('students')
-            navigate('/login')
-        } catch (error) {
-            alert(res.data.message || "Logout failed");
-            console.error("Logout failed:", error);
-        }
-    }
-
-    return (
-        <>
-            <header className="header">
-                <p id="header-title">Micro, Small and Medium Enterprises</p>
-                <div className="header-profile">
-                    <p><Link to="/">Home</Link></p>
-                    <p><Link to={userName ? null : '/login'}>{userName ? "Hi, " + userName : "Login"}</Link></p>
-                    {userName && <p onClick={handleLogout}>Logout</p>}
-                    <FontAwesomeIcon icon={faCircleUser} style={{ color: "#fff", fontSize: "30px" }} />
-                </div>
-            </header>
-        </>
-    );
-=======
 import {faCircleUser,faRightFromBracket} from "@fortawesome/free-solid-svg-icons";
 import "../styles/header.css";
 import { Link } from "react-router-dom";
@@ -107,7 +68,6 @@ const Header = ({ userName }) => {
       </header>
     </>
   );
->>>>>>> Stashed changes
 };
 
 export default Header;
